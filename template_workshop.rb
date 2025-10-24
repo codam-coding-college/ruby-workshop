1d705188beacb4a8a076df147da37c157eb78566
Lydia <159826376+1yd14@users.noreply.github.com>
2025-10-24 14:14:10 +0200

    Add files via upload

---
+++ template_workshop.rb
@@ -0,0 +1,25 @@
+# This template you can use to get started, fill in everything that is on the dots "..."
+
+puts "Welcome to the guessing game!"
+# you can do the rest!
+answer = gets.chomp.downcase
+
+#fill in the statement that has to be true
+if (...)
+    puts "Great, between what range is your number?"
+    puts "Minimum"
+    ...
+    puts "Maximimum"
+    ...
+    puts "I will guess your number between #{...} and #{...}"
+
+    #you can do the rest!
+    answer = gets.chomp.downcase
+    while (...)
+        if (...)
+
+        elsif (...)
+
+    end
+    puts "Nice, I got the right number!"
+end
